315a99612cdf04816216007f1fb1a25168e52f22
kirk <vonkratzmann@gmail.com>
2021-09-03 07:46:48 +1000

    started low power code

---
+++ lowpowertest/lowpowertest.ino
@@ -0,0 +1,50 @@
+#include "LowPower.h"
+
+void setup()
+{
+  pinMode(13,OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(13,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);
+  LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
+}
+
+#include "ArduinoLowPower.h"
+
+// Blink sequence number
+// Declare it volatile since it's incremented inside an interrupt
+volatile int repetitions = 1;
+
+// Pin used to trigger a wakeup
+const int pin = 8;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Set pin 8 as INPUT_PULLUP to avoid spurious wakeup
+  pinMode(pin, INPUT_PULLUP);
+  // Attach a wakeup interrupt on pin 8, calling repetitionsIncrease when the device is woken up
+  LowPower.attachInterruptWakeup(pin, repetitionsIncrease, CHANGE);
+}
+
+void loop() {
+  for (int i = 0; i < repetitions; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+  // Triggers an infinite sleep (the device will be woken up only by the registered wakeup sources)
+  // The power consumption of the chip will drop consistently
+  LowPower.sleep();
+}
+
+void repetitionsIncrease() {
+  // This function will be called once on device wakeup
+  // You can do some little operations here (like changing variables which will be used in the loop)
+  // Remember to avoid calling delay() and long running functions since this functions executes in interrupt context
+  repetitions ++;
+}
