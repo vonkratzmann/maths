bb98fe3f14c87620886cd9451ae2d9ce14c700aa
kirk <vonkratzmann@gmail.com>
2021-09-03 11:55:38 +1000

    test

--- lowpowertest/lowpowertest.ino
+++ lowpowertest/lowpowertest.ino
@@ -1,32 +1,33 @@
+// **** INCLUDES *****
 #include "LowPower.h"
 
+// Use pin 2 as wake up pin
+const int wakeUpPin = 2;
 
-// Pin used to trigger a wakeup
-const int pin = 8;
-
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  // Set pin 8 as INPUT_PULLUP to avoid spurious wakeup
-  pinMode(pin, INPUT_PULLUP);
-  // Attach a wakeup interrupt on pin 8, calling repetitionsIncrease when the device is woken up
-  LowPower.attachInterruptWakeup(pin, repetitionsIncrease, CHANGE);
+void wakeUp()
+{
+    // Just a handler for the pin interrupt.
 }
 
-void loop() {
-  for (int i = 0; i < repetitions; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-  }
-  // Triggers an infinite sleep (the device will be woken up only by the registered wakeup sources)
-  // The power consumption of the chip will drop consistently
-  LowPower.sleep();
+void setup()
+{
+    // Configure wake up pin as input.
+    // This will consumes few uA of current.
+    pinMode(wakeUpPin, INPUT);   
 }
 
-void repetitionsIncrease() {
-  // This function will be called once on device wakeup
-  // You can do some little operations here (like changing variables which will be used in the loop)
-  // Remember to avoid calling delay() and long running functions since this functions executes in interrupt context
-  repetitions ++;
+void loop() 
+{
+    // Allow wake up pin to trigger interrupt on low.
+    attachInterrupt(0, wakeUp, LOW);
+    
+    // Enter power down state with ADC and BOD module disabled.
+    // Wake up when wake up pin is low.
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+    
+    // Disable external pin interrupt on wake up pin.
+    detachInterrupt(0); 
+    
+    // Do something here
+    // Example: Read sensor, data logging, data transmission.
 }
