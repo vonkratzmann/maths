795ab7a4a90ba5bcc146b0d7a645b2c521b44c4c
kirk <vonkratzmann@gmail.com>
2021-09-12 08:33:13 +1000

    removes libaries form repo

--- ArduinoOutput/libraries/Adafruit-GFX-Library-master/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-c:\Users\vonkr\Documents\kirk\maths\ArduinoOutput\libraries\Adafruit-GFX-Library-master\glcdfont.c.o: \
- C:\Users\vonkr\Documents\arduino\libraries\Adafruit-GFX-Library-master\glcdfont.c
--- ArduinoOutput/libraries/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-c:\Users\vonkr\Documents\kirk\maths\ArduinoOutput\libraries\Wire\utility\twi.c.o: \
- C:\Program\ Files\ (x86)\arduino-1.8.15\hardware\arduino\avr\libraries\Wire\src\utility\twi.c \
- C:\Program\ Files\ (x86)\arduino-1.8.15\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\arduino-1.8.15\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\arduino-1.8.15\hardware\arduino\avr\variants\standard/pins_arduino.h \
- C:\Program\ Files\ (x86)\arduino-1.8.15\hardware\arduino\avr\libraries\Wire\src\utility\twi.h
